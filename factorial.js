f0f5107c172a537cdefa2111bcf1e342704942a2
EdwardR <janver77@gmail.com>
2025-12-10 16:44:36 -0500

    update

--- factorial.js
+++ factorial.js
@@ -2,18 +2,18 @@ const propmt = require('prompt-sync')();
 
 function factorial(n) {
     if (n < 0) {
-        return "'Ingrese un numero mayor a 0: \n";
+        return "Ingrese un numero mayor a 0:\n";
     }
     if (n === 0 || n === 1) {
         return 1;
     }
-    let result = 1;
+    let result = 1;5
     for (let i = 2; i <= n; i++) {
         result *= i;
     }
     return result;
 }
 
-const number = parseInt(propmt('Enter a non-negative integer: '), 10);
+const number = parseInt(propmt("Ingrese un numero mayor a 0: \n"));
 const result = factorial(number);
-console.log(`The factorial of ${number} is: ${result}`);
+console.log(`El factorial de ${number} es:\n\t\t\t ${result}`);
